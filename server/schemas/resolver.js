4cea8ec913e19a86c8d6271046fed40de2c85d36
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-06-25 12:54:25 -0400

    added login function

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -15,6 +15,22 @@ const resolvers = {
             const user = await User.create(args);
             const token = signToken(user);
             return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,6 +20,7 @@ const typeDefs = gql`
 
     type Mutation {
         createUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
     }
 `;
 
